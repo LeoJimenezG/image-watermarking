85ed5ecea131fdd15472180020a39196db9304e9
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-01-30 23:15:58 -0600

    initial watermark code

---
+++ watermark.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+
+class Watermark:
+    def __init__(self) -> None:
+        """
+           Initialize the object with preset settings
+           :return: returns None
+        """
+        self.image = None
