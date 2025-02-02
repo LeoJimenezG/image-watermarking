c6b8d9485246cc9e829d6076e4722ce2eef5fc29
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-02-01 21:28:45 -0600

    specify data types

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
-from tkinter import Tk, Canvas, Button
+from tkinter import Tk, Canvas, Button, PhotoImage
 from PIL import Image, ImageTk
+from typing import cast
 
 
 class UI:
@@ -9,26 +10,26 @@ class UI:
         :return: returns None
         """
 
-        self.rootSize = (1000, 1000)  # Width - Height
-        self.canvasSize = (1000, 800)  # Width - Height
-        self.font = ("Courier", 24, "normal")
+        self.rootSize: tuple = (1000, 1000)  # Width - Height
+        self.canvasSize: tuple = (1000, 800)  # Width - Height
+        self.font: tuple = ("Courier", 24, "normal")
 
-        self.root = Tk()
+        self.root: Tk = Tk()
         self.root.title("Image Watermarking")
         self.root.config(width=self.rootSize[0], height=self.rootSize[1], padx=5, pady=5)
         self.root.resizable(False, False)
 
-        self.canvas = None
-        self.originalImage = None
-        self.canvasImage = None
+        self.canvas: Canvas = cast(Canvas, None)
+        self.originalImage: Image = cast(Image, None)
+        self.canvasImage: PhotoImage = cast(PhotoImage, None)
 
-        self.selectImageButton = Button(self.root, text="Select Image", command="", font=self.font)
+        self.selectImageButton: Button = Button(self.root, text="Select Image", command="", font=self.font)
         self.selectImageButton.grid(column=0, row=0)
-        self.selectMarkButton = Button(self.root, text="Select Mark", command="", font=self.font)
+        self.selectMarkButton: Button = Button(self.root, text="Select Mark", command="", font=self.font)
         self.selectMarkButton.grid(column=1, row=0)
-        self.selectPosButton = Button(self.root, text="Select Position", command="", font=self.font)
+        self.selectPosButton: Button = Button(self.root, text="Select Position", command="", font=self.font)
         self.selectPosButton.grid(column=2, row=0)
-        self.saveButton = Button(self.root, text="Save Watermarked", command="", font=self.font)
+        self.saveButton: Button = Button(self.root, text="Save Watermarked", command="", font=self.font)
         self.saveButton.grid(column=3, row=0)
 
         # TODO: create and add the functions for each button
@@ -48,13 +49,13 @@ class UI:
                 self.canvas.grid(column=0, row=1, columnspan=4)
 
             if image.size[0] > self.canvasSize[0] or image.size[1] > self.canvasSize[1]:
-                resizedImage = image.resize(self.canvasSize)
+                resizedImage: Image = image.resize(self.canvasSize)
                 self.canvasImage = ImageTk.PhotoImage(resizedImage)
             else:
                 self.canvasImage = ImageTk.PhotoImage(image)
 
-            centerX = self.canvasSize[0] // 2
-            centerY = self.canvasSize[1] // 2
+            centerX: int = self.canvasSize[0] // 2
+            centerY: int = self.canvasSize[1] // 2
 
             self.canvas.delete("all")
 
