ca68e88f226857fa04866d5f9492459291604a7b
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-02-03 00:10:55 -0600

    main code

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 from ui import UI
-from watermark import Watermark
-from PIL import Image
 
 interface = UI()
-watermarkObj = Watermark()
-
-im = Image.open("img.jpeg")
-wk = Image.open("tomate.png")
-
-interface.set_image(image=im)
-mk = watermarkObj.watermark_image(im, wk, 0)
-interface.set_image(image=mk)
 interface.keep_open()
